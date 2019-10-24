07ac907067dd915b7cc612a9a870e1ac8e080072
Gary Johnson <garycjohnso@gmail.com>
2019-10-23 23:02:55 -0400

    Fix db creation issue

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
-
 class User(UserMixin, db.Model):
+
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True, nullable=True)
@@ -32,3 +34,5 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+db.create_all()
