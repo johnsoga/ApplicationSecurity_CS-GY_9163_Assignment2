720908925167d43489e793fc5fae20fbdb87e6b5
Gary Johnson <garycjohnso@gmail.com>
2019-10-24 00:26:13 -0400

    Fix user sign in expected id

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True, nullable=True)
+    auth_2fa = db.Column(db.String(10), index=True)
     password_hash = db.Column(db.String(128))
 
     def set_password(self, password):
