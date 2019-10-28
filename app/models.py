33d712a8770e469790cd4aab33cff96dc5753501
Gary Johnson <garycjohnso@gmail.com>
2019-10-27 21:20:02 -0400

    Refactor final submission

--- app/models.py
+++ app/models.py
@@ -23,15 +23,6 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     body = db.Column(db.String(140))
-#     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-#
-#     def __repr__(self):
-#         return '<Post {}>'.format(self.body)
-
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
